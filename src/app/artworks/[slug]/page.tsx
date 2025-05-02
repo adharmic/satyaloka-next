934f2cb3a2d02e3b41db3e254130046e7854eff9
Adithya Ajith <adithya@satyaloka.org>
2025-05-02 15:11:40 -0500

    Fix incorrect types

--- src/app/artworks/[slug]/page.tsx
+++ src/app/artworks/[slug]/page.tsx
@@ -3,9 +3,9 @@ import MdxTitle from "@/components/markdown/MdxTitle";
 import SectionHeader from "@/components/SectionHeader";
 import { getArtwork } from "@/utilities/content-manager";
 
-export default async function ArtworkSlug(params: Promise<{ params: { slug: string } }>) {
+export default async function ArtworkSlug(params: Promise<{ slug: string }>) {
   const p = await params
-  const frontmatter = getArtwork(p.params.slug);
+  const frontmatter = getArtwork(p.slug);
   return (
     <div className="flex flex-col">
       <div className="mb-8">
@@ -15,7 +15,7 @@ export default async function ArtworkSlug(params: Promise<{ params: { slug: stri
         {frontmatter?.title}
       </MdxTitle>
       <div className="h-full w-full rounded-xl">
-        <Image width={2000} height={2000} alt={p.params.slug} className="h-full w-full object-contain border-2 border-[var(--damask)]" src={frontmatter?.img || ""} />
+        <Image width={2000} height={2000} alt={p.slug} className="h-full w-full object-contain border-2 border-[var(--damask)]" src={frontmatter?.img || ""} />
       </div>
       <div className="text-end p-4 bg-[var(--damask)] w-full rounded-b-xl self-end text-[var(--paper)] mb-8">
         {`Published on ${new Date(frontmatter?.date || 0).toLocaleDateString()}`}
