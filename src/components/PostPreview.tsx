dd4a5fc73f3a606a5cd7a549535bfa782b5baafd
Adithya Ajith <adithya@satyaloka.org>
2025-05-03 06:14:34 -0500

    Add Links to images and titles

--- src/components/PostPreview.tsx
+++ src/components/PostPreview.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Button from "./Button";
 
 interface PostPreviewProps {
@@ -5,7 +6,7 @@ interface PostPreviewProps {
   title?: string,
   description?: string,
   img?: string,
-  link?: string,
+  link: string,
 }
 
 export default function PostPreview(props: PostPreviewProps) {
@@ -13,13 +14,13 @@ export default function PostPreview(props: PostPreviewProps) {
   return (
     <div className={"w-full flex flex-col md:flex-row gap-4 justify-evenly border-1 border-[var(--damask)] p-4 rounded-lg bg-[var(--secondary)] text-[var(--damask)] drop-shadow-xl" + (props.alt && " !bg-[var(--red)] text-[var(--paper)]")}>
       <div className="w-full flex flex-col gap-4 md:p-2 justify-center">
-        <div className="d-respira text-2xl font-bold">{props.title || "Here's what a sample blog title would look like."}</div>
+        <Link href={props.link} className="hover:underline transition-all d-respira text-2xl font-bold">{props.title || "Here's what a sample blog title would look like."}</Link>
         <div className="d-respira">{props.description || "And here's what that post's extended description might say. Lorem ipsum dolor sit amet. Something something something."}</div>
         <Button alt={props.alt} link={props.link || ""} title="Read More" />
       </div>
-      <div className="w-full border-1 border-[var(--damask)] h-64 rounded-lg">
+      <Link href={props.link} className="hover:border-[var(--paper)] transition-all w-full border-1 border-[var(--damask)] h-64 rounded-lg">
         <img alt={props.description} className="w-full h-full object-cover rounded-lg" src={props.img} />
-      </div>
+      </Link>
     </div>
   )
 }
--- src/components/SquareThumb.tsx
+++ src/components/SquareThumb.tsx
@@ -1,4 +1,5 @@
-import { SlLink } from "react-icons/sl"
+import { SlLink, SlArrowRight } from "react-icons/sl"
+import Link from "next/link"
 
 interface SquareThumbProps {
   title: string,
@@ -11,13 +12,15 @@ export default function SquareThumb(props: SquareThumbProps) {
   return (
     <div className={`drop-shadow-xl w-full lg:w-72 h-full lg:h-100 text-[var(--paper)] border-1 border-[var(--damask)] rounded-lg bg-[var(--red)] ${props.alt && "bg-[var(--secondary)]"}`}>
       <div className="h-full p-4 flex flex-col gap-2">
-        <img className={`w-full aspect-square object-cover rounded-lg border-1 ${props.alt && "border-[var(--damask)]" || "border-[var(--paper)]"}`} src={props.img} />
+        <Link href={props.link} className="w-full">
+        <img className={`hover:border-[var(--damask)] transition-all w-full aspect-square object-cover rounded-lg border-1 ${props.alt && "border-[var(--damask)] hover:border-[var(--paper)]" || "border-[var(--paper)]"}`} src={props.img} />
+        </Link>
         <div className="h-full d-respira text-xl flex flex-row items-center justify-between">
-          <div className={`self-start w-full ${props.alt && "text-[var(--damask)]" || ""}`}>
+          <Link href={props.link} className={`hover:underline transition-all hover:cursor-pointer self-start w-full ${props.alt && "text-[var(--damask)]" || ""}`}>
             {props.title}
-          </div>
+          </Link>
           <a href={props.link} className="transition-all hover:cursor-pointer active:scale-90 hover:bg-[var(--paper)] hover:text-[var(--damask)] self-end drop-shadow-lg w-12 min-w-12 aspect-square bg-[var(--damask)] rounded-full text-[var(--paper)] flex items-center justify-center text-2xl">
-            <SlLink />
+            <SlArrowRight />
           </a>
         </div>
       </div>
